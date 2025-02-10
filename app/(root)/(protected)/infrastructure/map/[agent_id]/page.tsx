6957a253060a935f0700321929b303a7e6df107c
Mohammednur <mamebb2023@gmail.com>
2025-02-10 08:11:18 +0300

    Refactor LineChart component to accept datasets, enhance chart rendering, and improve network usage display

--- app/(root)/(protected)/infrastructure/map/[agent_id]/page.tsx
+++ app/(root)/(protected)/infrastructure/map/[agent_id]/page.tsx
@@ -8,7 +8,8 @@ import { IAgent } from "@/models/agent.model";
 import { IMetric } from "@/models/metric.model";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
-import LineChart from "@/components/Charts/LineChart";
+import LineChart from "@/components/Charts/LineChart"; // Import the updated LineChart component
+import Image from "next/image";
 
 const Page = () => {
   const pathname = usePathname();
@@ -21,13 +22,13 @@ const Page = () => {
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(true);
 
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setUpdateTrigger((prev) => !prev);
-  //   }, 15000);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setUpdateTrigger((prev) => !prev);
+    }, 15000);
 
-  //   return () => clearInterval(interval);
-  // }, []);
+    return () => clearInterval(interval);
+  }, []);
 
   useEffect(() => {
     if (!agent_id) return;
@@ -62,15 +63,54 @@ const Page = () => {
     fetchData();
   }, [updateTrigger, agent_id]);
 
+  // Prepare data for the chart
   const chartLabels = metric?.metrics.map((m) =>
     new Date(m.timestamp).toLocaleTimeString()
   );
-  const cpuData = metric?.metrics.map((m) => m.cpu_usage);
-  const memoryData = metric?.metrics.map((m) => m.memory_usage);
-  const diskData = metric?.metrics.map((m) => m.disk_usage);
-  const networkData = metric?.metrics.map((m) => m.network_usage);
-  const download = metric?.metrics.map((m) => m.network_usage.download);
-  const upload = metric?.metrics.map((m) => m.network_usage.upload);
+
+  // Individual datasets
+  const cpuUsage = {
+    label: "CPU Usage (%)",
+    data: metric?.metrics.map((m) => m.cpu_usage) || [],
+    borderColor: "rgba(75, 192, 192, 1)",
+    backgroundColor: "rgba(75, 192, 192, 0.2)",
+  };
+
+  const memoryUsage = {
+    label: "Memory Usage (%)",
+    data: metric?.metrics.map((m) => m.memory_usage) || [],
+    borderColor: "rgba(153, 102, 255, 1)",
+    backgroundColor: "rgba(153, 102, 255, 0.2)",
+  };
+
+  const diskUsage = {
+    label: "Disk Usage (%)",
+    data: metric?.metrics.map((m) => m.disk_usage) || [],
+    borderColor: "rgba(255, 159, 64, 1)",
+    backgroundColor: "rgba(255, 159, 64, 0.2)",
+  };
+
+  const networkUsage = {
+    label: "Network Usage",
+    datasets: [
+      {
+        label: "Upload (%)",
+        data: metric?.metrics.map((m) => m.network_usage.upload) || [],
+        borderColor: "rgba(255, 99, 132, 1)",
+        backgroundColor: "rgba(255, 99, 132, 0.2)",
+      },
+      {
+        label: "Download (%)",
+        data: metric?.metrics.map((m) => m.network_usage.download) || [],
+        borderColor: "rgba(54, 162, 235, 1)",
+        backgroundColor: "rgba(54, 162, 235, 0.2)",
+      },
+    ],
+  };
+
+  // Array of individual datasets
+  const datasets = [cpuUsage, memoryUsage, diskUsage];
+
   return (
     <div className="flex flex-col flex-1">
       <Tracker />
@@ -91,67 +131,76 @@ const Page = () => {
           <div className="bg-white rounded-md p-3">
             <div className="flex items-center justify-between">
               <p className="text-gray-500/60">
-                public ip: <b>{agent?.device_info.public_ip}</b>
+                public ip:{" "}
+                <span className="font-bold">
+                  {agent?.device_info.public_ip}
+                </span>
               </p>
               <p className="text-gray-500/60">
-                id: <b>{agent?.agent_id}</b>
+                id: <span className="font-bold">{agent?.agent_id}</span>
               </p>
             </div>
 
-            <h3 className="h3 font-bold">{agent?.device_info.hostname}</h3>
-            <p className="text-gray-500/60">
-              Alises: <b>{agent?.device_info.node_name}</b>
-            </p>
+            <div className="flex items-center justify-between">
+              <div className="flex items-center gap-1">
+                <h3 className="h3 font-bold">{agent?.device_info.hostname}</h3>
+                <div
+                  className={`px-3 rounded-full ${
+                    metric?.metrics[metric?.metrics.length - 1].status ===
+                    "Online"
+                      ? "bg-green-500"
+                      : "bg-gray-500"
+                  }`}
+                >
+                  <p className="text-white text-sm">
+                    {metric?.metrics[metric?.metrics.length - 1].status}
+                  </p>
+                </div>
+              </div>
 
-            <Divider width="95" opacity="0.2" />
+              <div className="px-3 flex items-center gap-2">
+                <Image
+                  src={`/os/${agent?.device_info.system}${
+                    agent?.device_info.system === "Windows" &&
+                    `-${agent?.device_info.release}`
+                  }.png`}
+                  width={40}
+                  height={40}
+                  alt={`${agent?.device_info.system}-${agent?.device_info.release}`}
+                />
+                <div>
+                  <p className="text-gray-500/60">
+                    {agent?.device_info.system} {agent?.device_info.release}
+                  </p>
+                  <p className="text-gray-500/60">
+                    {agent?.device_info.version}
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <Divider width="90" opacity="0.2" />
           </div>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-2">
-            {metric && (
-              <>
-                {/* <div className="bg-white p-2 rounded-md">
-                  <LineChart
-                    labels={chartLabels || []}
-                    dataPoints={cpuData || []}
-                    title="CPU Usage (%)"
-                    borderColor="rgba(75, 192, 192, 1)"
-                    backgroundColor="rgba(75, 192, 192, 0.2)"
-                  />
-                </div>
-                <div className="bg-white p-2 rounded-md">
-                  <LineChart
-                    labels={chartLabels || []}
-                    dataPoints={memoryData || []}
-                    title="Memory Usage (%)"
-                    borderColor="rgba(153, 102, 255, 1)"
-                    backgroundColor="rgba(153, 102, 255, 0.2)"
-                  />
-                </div>
-                <div className="bg-white p-2 rounded-md"> */}
-                {/* <LineChart
-                  labels={chartLabels || []}
-                  dataPoints={[diskData!]}
-                  title="Disk Usage (%)"
-                  borderColor={["rgba(255, 159, 64, 1)"]}
-                  backgroundColor={["rgba(255, 159, 64, 0.2)"]}
-                /> */}
-                {/* </div> */}
-
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-2">
+            {datasets.map((dataset, index) => (
+              <div key={index} className="bg-white rounded-md p-3">
                 <LineChart
                   labels={chartLabels || []}
-                  dataPoints={[download!, upload!]}
-                  title="Network Usage (%)"
-                  borderColor={[
-                    "rgba(255, 99, 132, 1)",
-                    "rgba(0, 255, 100, 1)",
-                  ]}
-                  backgroundColor={[
-                    "rgba(255, 99, 132, 0.2)",
-                    "rgba(0, 255, 100, 0.2)",
-                  ]}
+                  datasets={[dataset]} // Pass all network datasets
+                  title={dataset.label}
                 />
-              </>
-            )}
+              </div>
+            ))}
+
+            {/* Network Usage Chart */}
+            <div className="bg-white rounded-md p-3">
+              <LineChart
+                labels={chartLabels || []}
+                datasets={networkUsage.datasets} // Pass both upload and download datasets
+                title="Network Usage"
+              />
+            </div>
           </div>
         </div>
       )}
--- components/Charts/LineChart.tsx
+++ components/Charts/LineChart.tsx
@@ -9,6 +9,7 @@ import {
   LineElement,
   Title,
   Tooltip,
+  Legend,
 } from "chart.js";
 
 // Register Chart.js components
@@ -18,58 +19,63 @@ ChartJS.register(
   PointElement,
   LineElement,
   Title,
-  Tooltip
+  Tooltip,
+  Legend
 );
 
 interface LineChartProps {
   labels: string[];
-  dataPoints: number[][];
+  datasets: {
+    label: string;
+    data: number[];
+    borderColor: string;
+    backgroundColor: string;
+  }[];
   title: string;
-  borderColor: string[];
-  backgroundColor: string[];
 }
 
-const LineChart = ({
-  labels,
-  dataPoints,
-  title,
-  borderColor,
-  backgroundColor,
-}: LineChartProps) => {
+const LineChart = ({ labels, datasets, title }: LineChartProps) => {
   const data = {
     labels,
-    datasets: [
-      {
-        label: title,
-        data: dataPoints,
-        borderColor,
-        backgroundColor,
-        tension: 0.4,
-      },
-    ],
+    datasets: datasets.map((dataset) => ({
+      ...dataset,
+      tension: 0.4,
+    })),
   };
 
   const options = {
     responsive: true,
+    interaction: {
+      mode: "index" as const,
+      intersect: false,
+    },
+    stacked: false,
     plugins: {
       legend: {
-        display: false, // Remove legend
+        display: false,
       },
       title: {
         display: true,
-        text: title, // Dynamic title
+        text: title,
       },
     },
     scales: {
       x: {
-        display: true,
+        display: false,
       },
       y: {
+        type: "linear" as const,
+        display: true,
+        position: "left" as const,
         min: 0,
         max: 100,
+        ticks: {
+          callback: function (value: number | string) {
+            return value === 0 || value === 100 ? value : "";
+          },
+        },
         title: {
           display: false,
-          text: "Usage (%)",
         },
       },
     },
