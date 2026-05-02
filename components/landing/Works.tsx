e29f384134ac91d059c852d96ba104146a7e1f88
Mohammednur <mamebb2023@gmail.com>
2026-05-02 16:36:29 +0300

    Update work component

--- components/landing/Works.tsx
+++ components/landing/Works.tsx
@@ -99,10 +99,12 @@ const Works = () => {
                 <AnimatePresence mode="wait">
                   {currentIndex === index && (
                     <motion.div
-                      initial={{ height: 0 }}
-                      animate={{ height: "100%" }}
-                      exit={{ height: 0 }}
-                      className="absolute bottom-0 left-0 w-[101%] bg-gradient-to-b from-transparent to-black/80 flex items-end">
+                      initial={{ opacity: 0, y: "100%" }}
+                      animate={{ opacity: 1, y: 0 }}
+                      exit={{ opacity: 0, y: "100%" }}
+                      transition={{ duration: 0.6, ease: "easeInOut" }}
+                      className="absolute inset-0 bg-gradient-to-b from-transparent to-black/80 flex items-end"
+                    >
 
                     </motion.div>
                   )}
